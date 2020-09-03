5ec270ada2b73b4e1e5b1483040156d1503a1a01
Carlos Doria <carlosdoria953@gmail.com>
2020-09-03 10:33:13 -0300

    New state was created to store the game status. Implemented in the 'handleClick' function an 'if' to check the game status. Reset of all states implemented in function 'resetSquares'. Created an 'h2' on return.

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -4,21 +4,31 @@ import Board from '../Board';
 
 function Game () {
     // state created to store the value of Squares
-    const [squaresBoard, setSquaresBoard] = useState(Array(9).fill(null))
+    const [squaresBoard, setSquaresBoard] = useState(Array(9).fill(null));
     // state created to check whose is the move ('X' or 'O')
-    const [xIsNext, setXIsNext] = useState(true)
+    const [xIsNext, setXIsNext] = useState(true);
+    const [statusGame, setStatusGame] = useState('Next Player: X');
 
     function handleClick(index){
         const square = squaresBoard
         // if created to check if the value of the array object is null
         if (square[index] != null) {return}
         square[index] = xIsNext ? 'X' : 'O'
-        setSquaresBoard(square)
-        setXIsNext(!xIsNext)
+        if (squaresBoard.every(element => element != null)){
+            setStatusGame('Match drawn')
+        } else if (xIsNext === true) {
+            setStatusGame('Next Player: O')
+        } else if (xIsNext === false) {
+            setStatusGame('Next Player: X')
+        }
+        setSquaresBoard(square);
+        setXIsNext(!xIsNext);
     }
 
     function resetSquares() {
-        setSquaresBoard(Array(9).fill(null))
+        setSquaresBoard(Array(9).fill(null));
+        setXIsNext(true);
+        setStatusGame('Next Player: X')
     }
     
     return(
@@ -26,6 +36,9 @@ function Game () {
             <h1>    
                 Tic-Tac-Toe Game
             </h1>
+            <h2>
+                {statusGame}
+            </h2>
             <Board 
                 value={squaresBoard}
                 onClick={(i) => handleClick(i)}
