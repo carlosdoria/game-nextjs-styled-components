7e45d9e756474cf069b0aaba1d5a27f787f96508
Carlos Doria <carlosdoria953@gmail.com>
2020-09-02 21:41:32 -0300

    Imported 'useState' to created two states. Created the function 'handleClick' to mark X or O if the Square's value is different from null. Created the function 'resetSquares' which returns th Squares value to the default(null). On return a 'h1' was created; on the Board the 'value' and 'onClick' props were passed; and the 'Reset' button was created with the 'onClick' event that calls the 'resetSquares' function.

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,10 +1,37 @@
-import Board from '../Board'
+import { useState } from 'react';
+
+import Board from '../Board';
 
 function Game () {
+    // state created to store the value of Squares
+    const [squaresBoard, setSquaresBoard] = useState(Array(9).fill(null))
+    // state created to check whose is the move ('X' or 'O')
+    const [xIsNext, setXIsNext] = useState(true)
+
+    function handleClick(index){
+        const square = squaresBoard
+        // if created to check if the value of the array object is null
+        if (square[index] != null) {return}
+        square[index] = xIsNext ? 'X' : 'O'
+        setSquaresBoard(square)
+        setXIsNext(!xIsNext)
+    }
+
+    function resetSquares() {
+        setSquaresBoard(Array(9).fill(null))
+    }
+    
     return(
-        <>
-            <Board />
-        </>
+        <div>
+            <h1>    
+                Tic-Tac-Toe Game
+            </h1>
+            <Board 
+                value={squaresBoard}
+                onClick={(i) => handleClick(i)}
+            />
+            <button onClick={() => resetSquares()}>Reset</button>
+        </ div>
     )
 }
 
