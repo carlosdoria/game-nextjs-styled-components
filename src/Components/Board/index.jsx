b4a0fcbea2676da06741e9623d78eeeca9cfb729
Carlos Doria <carlosdoria953@gmail.com>
2020-09-02 21:13:35 -0300

    Imported props from 'game/index' to 'board/index'. The 'renderSquare' function was created with an 'i' parameter that serves to select a value of the props, this function to render a Square, passing the 'value' and 'onClick' props. On return Squares renderings have beem replaced by the 'renderSquare' function.

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -1,25 +1,54 @@
+import { useState } from 'react';
+
 import Square from '../Square'
 
-function Board () {
+function Board (props) {
+    function renderSquare (i) {
+            return(
+                <Square 
+                    value={props.value[i]}
+                    onClick={() => props.onClick(i)}
+                />
+            );
+    }
+
     return(
-        <>
+        <div>
             <div>
-                <Square />
-                <Square />
-                <Square />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div>
-                <Square />
-                <Square />
-                <Square />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div>
-                <Square />
-                <Square />
-                <Square />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default Board
+
+{/* <>
+<div>
+    <Square onClick={() => props.click(0)} value={props.valueSquare[0]}/>
+    <Square onClick={() => props.click(1)} value={props.valueSquare[1]}/>
+    <Square onClick={() => props.click(2)} value={props.valueSquare[2]}/>
+</div>
+<div>
+    <Square />
+    <Square />
+    <Square />
+</div>
+<div>
+    <Square />
+    <Square />
+    <Square />
+</div>
+</> */}
